937320cfdb0e5afd0ae9288dd4f1c9209e700a9d
morry kolman <wttdotm@gmail.com>
2025-12-14 01:05:25 -0500

    add new megas

---
+++ fullDex2.js
@@ -0,0 +1,183 @@
+
+
+let ZA_DLC_MEGAS = {
+    'Absol-Mega-Z': {
+        fn: 'https://i.ibb.co/xKPzs3Px/image.png',
+        fs: 'https://i.ibb.co/xKPzs3Px/image.png',
+    },
+    'Baxcalibur-Mega': {
+        fn: 'https://i.ibb.co/6R27njjB/image.png',
+        fs: 'https://i.ibb.co/6R27njjB/image.png',
+    },
+    'Chimecho-Mega': {
+        fn: 'https://i.ibb.co/xrtV2JY/image.png',
+        fs: 'https://i.ibb.co/xrtV2JY/image.png',
+    },
+    'Crabominable-Mega': {
+        fn: 'https://i.ibb.co/xttc6Fz5/image.png',
+        fs: 'https://i.ibb.co/xttc6Fz5/image.png',
+    },
+    'Darkrai-Mega': {
+        fn: 'https://i.ibb.co/c9LBMhb/image.png',
+        fs: 'https://i.ibb.co/c9LBMhb/image.png',
+    },
+    'Garchomp-Mega-Z': {
+        fn: 'https://i.ibb.co/Z1NqzRXc/image.png',
+        fs: 'https://i.ibb.co/Z1NqzRXc/image.png',
+    },
+    'Glimmora-Mega': {
+        fn: 'https://i.ibb.co/4R1BXZMj/image.png',
+        fs: 'https://i.ibb.co/4R1BXZMj/image.png',
+    },
+    'Golisopod-Mega': {
+        fn: 'https://i.ibb.co/LX2R85wn/image.png',
+        fs: 'https://i.ibb.co/LX2R85wn/image.png',
+    },
+    'Golurk-Mega': {
+        fn: 'https://i.ibb.co/0Tcr7yz/image.png',
+        fs: 'https://i.ibb.co/0Tcr7yz/image.png',
+    },
+    'Heatran-Mega': {
+        fn: 'https://i.ibb.co/Y76mqL1z/image.png',
+        fs: 'https://i.ibb.co/Y76mqL1z/image.png',
+    },
+    'Lucario-Mega-Z': {
+        fn: 'https://i.ibb.co/HpBtyjqY/image.png',
+        fs: 'https://i.ibb.co/HpBtyjqY/image.png',
+    },
+    'Magearna-Mega': {
+        fn: 'https://i.ibb.co/7xQTPvgj/image.png',
+        fs: 'https://i.ibb.co/7xQTPvgj/image.png',
+    },
+    'Meowstic-F-Mega': {
+        fn: 'https://i.ibb.co/GbsYmXm/image.png',
+        fs: 'https://i.ibb.co/GbsYmXm/image.png',
+    },
+    'Raichu-Mega-X': {
+        fn: 'https://i.ibb.co/jkLGNV5x/image.png',
+        fs: 'https://i.ibb.co/jkLGNV5x/image.png',
+    },
+    'Raichu-Mega-Y': {
+        fn: 'https://i.ibb.co/PvcW5Dtd/image.png',
+        fs: 'https://i.ibb.co/PvcW5Dtd/image.png',
+    },
+    'Scovillain-Mega': {
+        fn: 'https://i.ibb.co/BvzYDf6/image.png',
+        fs: 'https://i.ibb.co/BvzYDf6/image.png',
+    },
+    'Staraptor-Mega': {
+        fn: 'https://i.ibb.co/351DPXgg/image.png',
+        fs: 'https://i.ibb.co/351DPXgg/image.png',
+    },
+    'Tatsugiri-Curly-Mega': {
+        fn: 'https://i.ibb.co/QvVG1MWn/image.png',
+        fs: 'https://i.ibb.co/QvVG1MWn/image.png',
+    },
+    'Zeraora-Mega': {
+        fn: 'https://i.ibb.co/Ps8BKPyy/image.png',
+        fs: 'https://i.ibb.co/Ps8BKPyy/image.png',
+    },
+    'Clefable-Mega': {
+        fn: 'https://i.ibb.co/mCMWRCdY/image.png',
+        fs: 'https://i.ibb.co/pBVM5Qg8/image.png'
+    },
+    'Victreebel-Mega': {
+        fn: 'https://i.ibb.co/60vfrDJm/image.png',
+        fs: 'https://i.ibb.co/60vfrDJm/image.png',
+    },
+    'Starmie-Mega': {
+        fn: 'https://i.ibb.co/cKB0ZQbZ/image.png',
+        fs: 'https://i.ibb.co/jvRSD4S6/image.png'
+    },
+    'Meganium-Mega': {
+        fn: 'https://i.ibb.co/0RYrbv2X/image.png',
+        fs: 'https://i.ibb.co/mCJqzyBy/image.png'
+    },
+    'Feraligatr-Mega': {
+        fn: 'https://i.ibb.co/Z6CPZ9W7/image.png',
+        fs: 'https://i.ibb.co/dwKnw27P/image.png'
+    },
+    'Skarmory-Mega': {
+        fn: 'https://i.ibb.co/zh15Qq2x/image.png',
+        fs: 'https://i.ibb.co/hx484z2c/image.png'
+    },
+    'Froslass-Mega': {
+        fn: 'https://i.ibb.co/LzGPJwJS/image.png',
+        fs: 'https://i.ibb.co/qY5CsgL2/image.png'
+    },
+    'Emboar-Mega': {
+        fn: 'https://i.ibb.co/rK0HNbPM/image.png',
+        fs: 'https://i.ibb.co/fY4k0tTY/image.png'
+    },
+    'Excadrill-Mega': {
+        fn: 'https://i.ibb.co/yBpjCRxc/image.png',
+        fs: 'https://i.ibb.co/cXrj0qDx/image.png'
+    },
+    'Scolipede-Mega': {
+        fn: 'https://i.ibb.co/rKNq4Jnv/image.png',
+        fs: 'https://i.ibb.co/jv1nnVvj/image.png'
+    },
+    'Scrafty-Mega': {
+        fn: 'https://i.ibb.co/LXHsYBWn/image.png',
+        fs: 'https://i.ibb.co/svZc9mL2/image.png'
+    },
+    'Eelektross-Mega': {
+        fn: 'https://i.ibb.co/ZprhFc3K/image.png',
+        fs: 'https://i.ibb.co/tgFrDvn/image.png',
+    },
+    'Chandelure-Mega': {
+        fn: 'https://i.ibb.co/mFD6477w/image.png',
+        fs: 'https://i.ibb.co/b5jQs8hh/image.png'
+    },
+    'Chesnaught-Mega': {
+        fn: 'https://i.ibb.co/tPk4rN91/image.png'
+    },
+    'Delphox-Mega': {
+        fn: 'https://i.ibb.co/mFGfvBWc/image.png'
+    },
+    'Greninja-Mega': {
+        fn: 'https://i.ibb.co/zTQZF0zg/image.png'
+    },
+    'Pyroar-Mega': {
+        fn: 'https://i.ibb.co/6RcQMnGr/image.png',
+        fs: 'https://i.ibb.co/Jwcn0c9w/image.png',
+    },
+    'Floette-Mega': {
+        fn: 'https://i.ibb.co/RTqf9vw8/image.png',
+        fs: 'https://i.ibb.co/4xqDJt8/image.png'
+    },
+    'Malamar-Mega': {
+        fn: 'https://i.ibb.co/hFdCQTdN/image.png',
+        bn: 'https://i.ibb.co/hF7MR2VT/image.png'
+    },
+    'Barbaracle-Mega': {
+        fn: 'https://i.ibb.co/WNcps5Sv/image.png',
+        fs: 'https://i.ibb.co/rK9bmCZw/image.png'
+    },
+    'Dragalge-Mega': {
+        fn: 'https://i.ibb.co/849Pcfhn/image.png',
+        fs: 'https://i.ibb.co/hRwTCRNS/image.png'
+    },
+    'Hawlucha-Mega': {
+        fn: 'https://i.ibb.co/HT27Mypv/image.png',
+        fs: 'https://i.ibb.co/8nRN9krW/image.png',
+    },
+    'Zygarde-Mega': {
+        fn: 'https://i.ibb.co/Wv29jpt8/image.png',
+        fs: 'https://i.ibb.co/p65J1nZy/image.png'
+    },
+    'Drampa-Mega': {
+        fn: 'https://i.ibb.co/TySjQy6/image.png',
+        fs: 'https://i.ibb.co/m5fp6Rfr/image.png'
+    },
+    'Falinks-Mega': {
+        fn: 'https://i.ibb.co/sJvxDq67/image.png',
+        fs: 'https://i.ibb.co/qLh6n38J/image.png'
+    }
+};
+
+let megaDex = {}
+for (let mega in ZA_DLC_MEGAS) {
+    megaDex[mega] = [ZA_DLC_MEGAS[mega].fn, ZA_DLC_MEGAS[mega].fs];
+}
+console.log(megaDex);
